878720bb50842b33d9405e2fee8a6b994ac45fde
shikha82 <shikha.das1@gmail.com>
2018-09-24 10:53:55 +0530

    Initial Commit

---
+++ javaFirst.js
@@ -0,0 +1,7 @@
+var firstName = prompt('what is your first name');
+var lastName = prompt('what is your last name');
+var age = prompt('what is your age');
+alert('Your full name is, ' + firstName + ' ' + lastName);
+console.log('Your full name is, ' + firstName + ' ' + lastName);
+alert('Your age is, ' + age);
+console.log('Your age is, ' + age);
